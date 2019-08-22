6bdf83e21555d6784540bf6523f8dd0b052dc885
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2019-08-22 08:32:41 -0500

    Update from local

--- ploteador_xd.py
+++ ploteador_xd.py
@@ -1,8 +1,10 @@
 from Plotter import Plotter
 
-p = Plotter('./Example_Files/linear.txt')
+p = Plotter('/home/juan/Proyectos/Simulacion/Proyecto/prueba_L32_a02_t1e8.txt')
+p.log_x, p.log_y = True, True
+p.frequency(no_title=True)
 
-p.scatter()
+
 
 
 
