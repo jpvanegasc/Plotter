7694f63f15f6263486a8fdadd0b02ebb885f7e79
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2019-08-23 09:17:38 -0500

    Merged from data_editor

--- ploteador_xd.py
+++ ploteador_xd.py
@@ -1,12 +1,5 @@
 from Plotter import Plotter
 
-p = Plotter('/home/juan/Proyectos/Simulacion/Proyecto/prueba_L32_a02_t1e8.txt')
-p.log_x, p.log_y = True, True
-p.frequency(no_title=True)
-
-
-
-
-
-
+p = Plotter('./Example_Files/3_col_lin_ran.txt', column_num = 3)
+p.lines(reg = 1, no_title = True)
 
