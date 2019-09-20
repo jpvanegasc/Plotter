d3995613a647ec518ad67ae980edbdf9111687bc
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2019-09-20 15:04:33 -0500

    Update

--- ploteador_xd.py
+++ ploteador_xd.py
@@ -1,26 +1,11 @@
 from Plotter import Plotter
 import os
-"""
-#path = '/home/juan/Proyectos/Automatas_Sismicos/Resultados/Raw_Files/L32/'
-path = '/home/juan/Proyectos/Automatas_Sismicos/Programas/'
-docs = sorted(os.listdir(path))
-#print(docs)
 
-for i, doc in enumerate(docs):
-	if doc.split('.')[-1] != 'txt':
-		continue
-	#print(doc)
-	p = Plotter(path+doc)
-	p.log_x = True
-	p.log_y = True
-	#if i != len(docs)-1:
-	#	p.multiple_graphs = True
-	p.frequency(no_title = True, label=doc)
+path = '/home/juan/Documentos/UN/7_S/Termo_Ex/04_9/diodo.txt'
 
+p = Plotter(path)
 
-
-"""
-p = Plotter('./Example_Files/termo.txt')
+p.x = ('T', r'C^o')
+p.y = ('U', r'V')
 p.scatter(reg=1)
-"""
-"""
+
